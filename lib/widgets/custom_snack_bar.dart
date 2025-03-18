cd2457b05a65febe2d82da13db7aefecde22db31
Lex <lexer.sodela@corp.mekongnet.com.kh>
2025-03-18 14:17:00 +0700

    Add CustomSnackBar widget with show method for displaying messages

--- lib/widgets/custom_snack_bar.dart
+++ lib/widgets/custom_snack_bar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'custom_text.dart';
+
 /// A custom widget of [SnackBar]
 class CustomSnackBar extends SnackBar {
   /// Creates an instance of [CustomSnackBar]
@@ -12,12 +14,28 @@ class CustomSnackBar extends SnackBar {
     SnackBarBehavior super.behavior = SnackBarBehavior.floating,
     super.margin,
     super.elevation,
+    super.width,
   }) : super(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-            side: BorderSide(
-              color: borderColor ?? Colors.black,
-            ), // Customize the border here
-          ),
-        );
+         shape: RoundedRectangleBorder(
+           borderRadius: BorderRadius.circular(12),
+           side: BorderSide(
+             color: borderColor ?? Colors.black,
+           ), // Customize the border here
+         ),
+       );
+
+  /// A method to show the snackbar
+  static void show(BuildContext context, {required String message}) {
+    final theme = Theme.of(context);
+    final surface = theme.colorScheme.surface;
+    final inverseSurface = theme.colorScheme.onSurface;
+    final scaffoldMessengerContext = ScaffoldMessenger.of(context);
+    scaffoldMessengerContext.showSnackBar(
+      CustomSnackBar(
+        backgroundColor: surface,
+        borderColor: inverseSurface,
+        content: CustomText(message),
+      ),
+    );
+  }
 }
